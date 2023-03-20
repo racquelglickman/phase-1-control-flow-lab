d0716e768075c31c3e63ce95deda1cfffac106cb
Racquel Glickman <racquel.glickman@gmail.com>
2023-03-20 14:15:30 -0400

    complete control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(numberOfFeet){
+  if (numberOfFeet <= 400) {
+    return 'This one is on me!'
+  } else if (numberOfFeet > 400 && numberOfFeet <= 2000){
+      return 'That will be twenty bucks.'
+  } else if (numberOfFeet > 2000 && numberOfFeet <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if (numberOfFeet > 2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(nameOfCity){
+  const returnMessage = nameOfCity === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  return returnMessage;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response;
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.'
+      break;
+    case 'not as generous':
+      response = 'Thank you.'
+      break;
+    default:
+      response = 'Bye.'
+      break;
+  }
+  return response;
 }
